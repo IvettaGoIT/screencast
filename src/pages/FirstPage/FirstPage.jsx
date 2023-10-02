9735dbc86b3b29fa423dca12a8ee19e48e365572
Ivetta Dashkova <ivettafsd@gmail.com>
2023-10-02 15:42:09 +0200

    add title to page

--- src/pages/FirstPage/FirstPage.jsx
+++ src/pages/FirstPage/FirstPage.jsx
@@ -4,6 +4,7 @@ import example from '../../assets/example.png';
 const FirstPage = () => {
   return (
     <Container>
+      <Title>First Page</Title>
       <StyledImage src={example} alt="Example" />
     </Container>
   );
