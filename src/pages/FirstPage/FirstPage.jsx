858a5187b41b166aae28e66fa4e3dffd36c1dd37
Ivetta Dashkova <ivettafsd@gmail.com>
2023-10-02 12:17:39 +0200

    add subtitle to first page

--- src/pages/FirstPage/FirstPage.jsx
+++ src/pages/FirstPage/FirstPage.jsx
@@ -1,10 +1,11 @@
-import { Container, Title, StyledImage } from './FirstPage.styled';
+import { Container, Title, Subtitle, StyledImage } from './FirstPage.styled';
 import example from '../../assets/example.png';
 
 const FirstPage = () => {
   return (
     <Container>
       <Title>First Page</Title>
+      <Subtitle>Extra description</Subtitle>
       <StyledImage src={example} alt="Example" />
     </Container>
   );
